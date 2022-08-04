23de201d071a512f45fe00fd879731c543d5c55e
anandhi2017 <anandhi@systimanx.com>
2022-08-04 19:28:01 +0530

    ui home page

--- pages/index.vue
+++ pages/index.vue
@@ -104,11 +104,155 @@
 
         </section>
         <section class="problem_statement">
-            <div class="h1_title has-text-centered">
-                <h1 class="">
-                    <span>Problem</span>
-                    <span>Statement</span>
-                </h1>
+            <div class="container">
+                <div class="h1_title has-text-centered">
+                    <h1 class="">
+                        <span>Problem</span>
+                        <span>Statement</span>
+                    </h1>
+                </div>
+                <ul class="prb_stmt">
+                    <li>
+                        <div class="round_circle">
+                            <img src="@/assets/img/Save_Environment.png" />
+                        </div>
+                        <p>Environment</p>
+                    </li>
+                    <li>
+                        <div class="round_circle">
+                            <img src="@/assets/img/stoplights-fill.png" />
+                        </div>
+                        <p>Traffic</p>
+                    </li>
+                    <li>
+                        <div class="round_circle">
+                            <img src="@/assets/img/mortarboard-fill.png" />
+                        </div>
+                        <p>Education</p>
+                    </li>
+                    <li>
+                        <div class="round_circle">
+                            <img src="@/assets/img/Technology.png" />
+                        </div>
+                        <p>Technology</p>
+                    </li>
+                    <li>
+                        <p>
+                            <nuxt-link to="/" class="click_here">Click here </nuxt-link>to see more details
+                        </p>
+                    </li>
+                </ul>
+            </div>
+        </section>
+        <section class="partners">
+            <div class="container">
+                <div class="has-text-centered">
+                    <h1 class="single_title_line ">Partners</h1>
+                    <div class="columns is-multiline is-mobile mt-3">
+                        <div class="column is-4-desktop is-12-mobile is-6-tablet">
+                            <p>Our Institutional Partner</p>
+                            <img src="@/assets/img/home/partner1.png" class="partner1_img" />
+                        </div>
+                        <div class="column is-4-desktop is-12-mobile is-6-tablet">
+                            <p>Promotional Partner</p>
+                            <img src="@/assets/img/home/partner2.png" class="partner2_img" />
+                        </div>
+                        <div class="column is-4-desktop is-12-mobile is-6-tablet">
+                            <p>Eco System Partner</p>
+                            <img src="@/assets/img/home/partner3.png" class="partner3_img" />
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </section>
+        <section class="sponsors">
+            <div class="container">
+                <div class="has-text-centered">
+                    <div class="h1_title has-text-centered">
+                        <h1 class="">
+                            <span>Sponsors</span>
+                            <span>Us</span>
+                        </h1>
+                    </div>
+                    <div class="s_single_txt">
+                        <p>Sponsor this event to make your brand known among the change makers!
+                        </p>
+                    </div>
+                    <div>
+                        <b-button>
+                            <nuxt-link to="about"><span class="check_out">Check Our</span> </nuxt-link>
+                            <span class="sponsr_btn"> Sponsorship packages </span>
+                        </b-button>
+                    </div>
+                </div>
+            </div>
+        </section>
+        <section class="sponsors">
+            <div class="container">
+                <div class="about_us">
+                    <div class="has-text-centered">
+                        <div class="h1_title has-text-centered">
+                            <h1 class="">
+                                <span>About</span>
+                                <span>Us</span>
+                            </h1>
+                        </div>
+                        <div class="s_single_txt">
+                            <p>We are a Volunteer Based Community, working for the benefit of the society around us. We
+                                leverage the strength of our community members
+                                technical, industry and business knowledge for the betterment of Indian cities. We, the
+                                volunteers, as well wishers of Madurai, are passionate about
+                                bringing change to Madurai by leveraging technology.
+                            </p>
+                        </div>
+                    </div>
+                    <div class="columns is-multiline is-mobile">
+                        <div class="column is-6-desktop is-12-mobile is-6-tablet">
+                            <h2 class="about_h2">As a Community, We
+                            </h2>
+                            <ul>
+                                <li>
+                                    Conduct Knowledge sharing sessions
+                                </li>
+                                <li>
+                                    Enable discussions on new technologies
+                                </li>
+                                <li>
+                                    Conduct events like Hackathon, Demo Days, Startup
+                                    Events, Job Fairs, Mentorship for startups etc.
+                                </li>
+                            </ul>
+
+                        </div>
+                        <div class="column is-6-desktop is-12-mobile is-6-tablet">
+                            <img src="@/assets/img/home/abt1.png" class="abt1" />
+                        </div>
+                    </div>
+                    <div class="columns is-multiline is-mobile">
+                        <div class="column is-6-desktop is-12-mobile is-6-tablet">                           
+
+                            <img src="@/assets/img/home/abt2.png" class="abt2" />
+                        </div>
+                        <div class="column is-6-desktop is-12-mobile is-6-tablet">
+                            
+                            <ul>
+                                <li>
+                                  Provide Opportunities for businesses to connect with clients
+  
+                                </li>
+                                <li>
+                                  Provide Opportunities for individuals looking to freelance or get into a job
+                                </li>
+                                <li>
+                                  Provide Networking opportunities for Technology Professionals, 
+                                  Freelancers, Startups, Established Businesses, 
+                                  Educational Institutions and Students
+                                  
+                                </li>
+                            </ul>
+                        </div>
+                    </div>
+                </div>
             </div>
         </section>
     </section>
