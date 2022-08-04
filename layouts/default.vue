5fe75f2634bde330d0832870d6c16b3433d63ef6
Alakar <alakar@systimanx.com>
2022-08-04 16:14:16 +0530

    init

---
+++ layouts/default.vue
@@ -0,0 +1,72 @@
+<template>
+  <div>
+    <b-navbar>
+      <template #brand>
+        <b-navbar-item tag="router-link" :to="{ path: '/' }">
+          <img src="@/assets/logo.png"
+            alt="Tech Community, Madurai, IT, Hackathon 2022, Volunteer Based Community, Hackathon, Demo Days, Startup Events, Job Fairs, Mentorship for startups, Networking, new technologies">
+        </b-navbar-item>
+      </template>
+      <template #start>
+
+      </template>
+
+      <template #end>
+        <b-navbar-item href="#">
+          Home
+        </b-navbar-item>
+        <b-navbar-item href="#">
+          Documentation
+        </b-navbar-item>
+        <b-navbar-dropdown label="Info">
+          <b-navbar-item href="#">
+            About
+          </b-navbar-item>
+          <b-navbar-item href="#">
+            Contact
+          </b-navbar-item>
+        </b-navbar-dropdown>
+      </template>
+    </b-navbar>
+
+    <section class="main-content columns">
+      <aside class="column is-2 section">
+        <p class="menu-label is-hidden-touch">
+          General
+        </p>
+        <ul class="menu-list">
+          <li v-for="(item, key) of items" :key="key">
+            <nuxt-link :to="item.to" exact-active-class="is-active">
+              <b-icon :icon="item.icon" /> {{ item.title }}
+            </nuxt-link>
+          </li>
+        </ul>
+      </aside>
+
+      <div class="container column is-10">
+        <nuxt />
+      </div>
+    </section>
+  </div>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      items: [
+        {
+          title: 'Home',
+          icon: 'home',
+          to: { name: 'index' }
+        },
+        {
+          title: 'Inspire',
+          icon: 'lightbulb',
+          to: { name: 'inspire' }
+        }
+      ]
+    }
+  }
+}
+</script>
---
+++ nuxt.config.js
@@ -0,0 +1,69 @@
+
+export default {
+  mode: 'universal',
+  // Server config
+  server: {
+    port: process.env.PORT || 3010,
+    host: '0.0.0.0',
+    timing: false
+  },
+  /*
+  ** Headers of the page
+  */
+  head: {
+    title: 'Madurai Hackathon 2022',
+    meta: [
+      { charset: 'utf-8' },
+      { name: 'viewport', content: 'width=device-width, initial-scale=1' },
+      { hid: 'description', name: 'description', content: 'Madurai Tech Community - Volunteer Based Community, working for the benefit of the society around us.' },
+      { hid: 'keywords', name: 'keywords', content: 'Tech Community, Madurai, IT, Hackathon 2022, Volunteer Based Community, Hackathon, Demo Days, Startup Events, Job Fairs, Mentorship for startups, Networking, new technologies' },
+    ],
+    link: [
+      { rel: 'icon', type: 'image/x-icon', href: '/favicon.png' }
+    ]
+  },
+  /*
+  ** Customize the progress-bar color
+  */
+  loading: { color: '#fff' },
+  /*
+  ** Global CSS
+  */
+  css: [
+  ],
+  /*
+  ** Plugins to load before mounting the App
+  */
+  plugins: [
+  ],
+  /*
+  ** Nuxt.js dev-modules
+  */
+  buildModules: [
+  ],
+  /*
+  ** Nuxt.js modules
+  */
+  modules: [
+    // Doc: https://buefy.github.io/#/documentation
+    'nuxt-buefy',
+    // Doc: https://axios.nuxtjs.org/usage
+    '@nuxtjs/axios',
+  ],
+  /*
+  ** Axios module configuration
+  ** See https://axios.nuxtjs.org/options
+  */
+  axios: {
+  },
+  /*
+  ** Build configuration
+  */
+  build: {
+    /*
+    ** You can extend webpack config here
+    */
+    extend(config, ctx) {
+    }
+  }
+}
