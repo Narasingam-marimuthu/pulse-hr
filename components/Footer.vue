633c0aa51896609272a7fb8ab0a1211467734718
Alakar <alakar@systimanx.com>
2022-08-04 18:23:37 +0530

    Empty page route created

--- components/Footer.vue
+++ components/Footer.vue
@@ -5,8 +5,8 @@
                 <div class="column is-3-desktop is-12-mobile is-6-tablet">
                     <div class="footer_block_one">
                         <h4>Contact us</h4>
-                        <p> <img src="" class=""/> 938466737</p>
-                        <p>  <img src="" class=""/>247, SLC2, Anna Nagar,
+                        <p> <img src="" class="" /> 938466737</p>
+                        <p> <img src="" class="" />247, SLC2, Anna Nagar,
                             Madurai - 625020
                         </p>
                     </div>
@@ -23,56 +23,70 @@
                     <div class="footer_block_three">
                         <h4>Useful Links</h4>
                         <p>
-                            <nuxt-link to="about" class="">About </nuxt-link>
+                            <nuxt-link to="/about" class="">About </nuxt-link>
                         </p>
                         <p>
-                            <nuxt-link to="sponsors" class="">Sponsors 2022</nuxt-link>
+                            <nuxt-link to="/sponsors" class="">Sponsors 2022</nuxt-link>
                         </p>
                         <p>
-                            <nuxt-link to="problem_statement" class="">Problem statement</nuxt-link>
+                            <nuxt-link to="/problem-statement" class="">Problem statement</nuxt-link>
                         </p>
                         <p>
-                            <nuxt-link to="register" class="">Register </nuxt-link>
+                            <nuxt-link to="/register" class="">Register </nuxt-link>
                         </p>
                     </div>
                 </div>
                 <div class="column is-4-desktop is-12-mobile is-6-tablet">
                     <div class="footer_block_four">
                         <div class="is-flex">
-                        <img src="@/assets/logo.png" class="logo_img"/>
-                        <div class="">
-                            <h3 class="">
-                             <span>  Madurai  </span> 
-                             <span>    Tech  </span> 
-                                <span>   community  </span> </h3>
+                            <img src="@/assets/logo.png" class="logo_img" />
+                            <div class="">
+                                <h3 class="">
+                                    <span> Madurai </span>
+                                    <span> Tech </span>
+                                    <span> community </span>
+                                </h3>
                             </div>
                         </div>
                         <p class="mb-4">A high-quality solution through collaboration between experts</p>
                         <div class="is-flex">
                             <p>Follow us on</p>
-                           <a href="" ><img src="" class=""/></a>
-                           <a href="" ><img src="" class=""/></a>
-                           <a href="" ><img src="" class=""/></a>
-                           <a href="" ><img src="" class=""/></a>
-                           <a href="" ><img src="" class=""/></a>
-                           <a href="" ><img src="" class=""/></a>
+                            <a href=""><img src="" class="" /></a>
+                            <a href=""><img src="" class="" /></a>
+                            <a href=""><img src="" class="" /></a>
+                            <a href=""><img src="" class="" /></a>
+                            <a href=""><img src="" class="" /></a>
+                            <a href=""><img src="" class="" /></a>
                         </div>
                     </div>
                 </div>
             </div>
             <div class=" copy_rights">
                 <div class="columns is-multiline is-mobile">
-                    <div class="column is-7-desktop is-12-mobile is-7-tablet has-text-right">                    
-                <p class="">&#169; Madurai Tech Community 
-                    <a href=""><img src="@/assets/img/footer_top.png"/> </a></p>
-                </div>
-                <div class="column is-5-desktop is-12-mobile is-5-tablet">
-                <p class="has-text-right">Design and Developed by <a href="" target="_blank">
-                    <b>  SystimaNX IT Solutions</b></a></p>
-                </div>
+                    <div class="column is-7-desktop is-12-mobile is-7-tablet has-text-right">
+                        <p class="">&#169; Madurai Tech Community
+                            <a @click="scrollToTop"><img src="@/assets/img/footer_top.png" /> </a>
+                        </p>
+                    </div>
+                    <div class="column is-5-desktop is-12-mobile is-5-tablet">
+                        <p class="has-text-right">Design and Developed by <a href="https://systimanx.com"
+                                target="_blank">
+                                <b> SystimaNX IT Solutions Pvt Ltd</b></a></p>
+                    </div>
                 </div>
 
             </div>
         </div>
     </section>
 </template>
+
+<script>
+export default {
+    methods: {
+        scrollToTop() {
+            window.scrollTo({ top: 0, behavior: 'smooth' })
+            // document.body.scrollTop = document.documentElement.scrollTop = 0;
+        }
+    }
+}
+</script>
