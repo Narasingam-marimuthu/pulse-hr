19cdb519bf658823d4b88f966e8dfd12d2e3e970
Ramesh <gv.ramesh2003@gmail.com>
2022-08-05 14:33:04 +0530

    About hackathon page desing - In-Progress

--- pages/about-hackathon/index.vue
+++ pages/about-hackathon/index.vue
@@ -1,46 +1,51 @@
 <template>
-    <section class="welcome_block">
+    <section class="">
         <div class="container">
+            <div class="heading_style has-text-centered my-5">
+                <h3 class="about_hackathon_header">
+                    <span> What is </span>
+                    <span> Madurai Hackathon 2022 </span>
+                </h3>
+            </div>
             <div class="columns is-multiline is-mobile">
                 <div class="column is-12-desktop is-12-mobile is-6-tablet">
-                    <div class="welcome_main_title">
-                        <h3 class="">
-                            <span> What is </span>
-                            <span> Madurai Hackathon 2022 </span>
-                        </h3>
-                    </div>
-                    <p class="content">It is an event to build innovative solutions that position Madurai as a smarter,
-                        resilient, and sustainable city for living, studying, accessing public services and conducting
-                        business.
+                    <p class="is-size-6 primary_color">It is an event to build innovative solutions that position
+                        Madurai as a smarter, resilient, and sustainable city for living, studying, accessing public
+                        services and conducting business.
                     </p>
-                    <p class="content">
+                    <p class="is-size-6 primary_color">
                         If you are an established business group or a start-up operating in this field, we invite you to
-                        partner with us to make this event successful.
-                        This is an Open Hackathon. The execution of the event will follow the below structure.
+                        partner with us to make this event successful. This is an Open Hackathon. The execution of the
+                        event will follow the below structure.
                     </p>
                 </div>
             </div>
             <div class="columns is-multiline is-mobile">
                 <div class="column is-6-desktop is-12-mobile is-6-tablet">
-                    <p>Problem statements will be published.</p>
-                    <p>Participants are encouraged to form teams with other participants.</p>
-                    <p>Hackathon organizers will also run Road Show session (either online or offline) to help
-                        participants create valuable solutions</p>
-                    <p>Hackathon offline event runs from 12th August to 14th August 2022.</p>
-                    <p>Hackathon work will start on 13th August Saturday morning and end by 14th August Sunday evening.
-                    </p>
+                    <ul>
+                        <li>Problem statements will be published.</li>
+                        <li>Participants are encouraged to form teams with other participants.</li>
+                        <li>Hackathon organizers will also run Road Show session (either online or offline) to help
+                            participants create valuable solutions</li>
+                        <li>Hackathon offline event runs from 12th August to 14th August 2022.</li>
+                        <li>Hackathon work will start on 13th August Saturday morning and end by 14th August Sunday
+                            evening.
+                        </li>
+                    </ul>
                 </div>
                 <div class="column is-6-desktop is-12-mobile is-6-tablet">
-                    <p>The discussion forum will open. Participants can join the discussion forums to get clarifications
-                        and guidance from the mentors and the community</p>
-                    <p>Participants can start working on their solutions from the day the problem statements are
-                        published. A detailed understanding of the problem area is essential to create a valuable and
-                        winning solution.</p>
-                    <p>Industry experts will be assigned as mentors to teams participating in this hackathon.</p>
-                    <p>12th August Friday evening is for networking, team formation if you want to strengthen your team,
-                        and for ice breaking.</p>
-                    <p>Hackathon offline event will bring together all the teams under one roof to finalize the
-                        solutions and present them to the jury for evaluation.</p>
+                    <ul>
+                        <li>The discussion forum will open. Participants can join the discussion forums to get
+                            clarifications and guidance from the mentors and the community</li>
+                        <li>Participants can start working on their solutions from the day the problem statements are
+                            published. A detailed understanding of the problem area is essential to create a valuable
+                            and winning solution.</li>
+                        <li>Industry experts will be assigned as mentors to teams participating in this hackathon.</li>
+                        <li>12th August Friday evening is for networking, team formation if you want to strengthen your
+                            team, and for ice breaking.</li>
+                        <li>Hackathon offline event will bring together all the teams under one roof to finalize the
+                            solutions and present them to the jury for evaluation.</li>
+                    </ul>
                 </div>
             </div>
             <div class="columns is-multiline is-mobile">
