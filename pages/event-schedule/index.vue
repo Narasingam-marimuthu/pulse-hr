c66fa785e860362b2ac19ee50eaf49468c563947
anandhi2017 <anandhi@systimanx.com>
2022-08-05 14:27:23 +0530

    event schedule page completed ui

--- pages/event-schedule/index.vue
+++ pages/event-schedule/index.vue
@@ -1,16 +1,111 @@
 <template>
+  <section class="event_schedule">
+    <div class="h1_title has-text-centered">
+      <h1 class="">
+        <span>Event </span>
+        <span>Schedule</span>
+      </h1>
+    </div>
+    <div class="container">
+      <div class="columns is-multiline is-mobile">
+        <div class="column is-3-desktop is-12-mobile is-3-tablet">
+          <div class="time_line_event">
+            <div class="event_item_one">
+              <div class="event_flag_bg_img event_item_round">
+                <p class="has-text-white event_day_txt"> Day 1</p>
+                <p class="brand_color event_date_txt">Friday, August 12</p>
+              </div>
+              <div class="event_list">
+                <ul>
+                  <li>
+                    Arrive at the venue
+                  </li>
+                  <li>
+                    Team Registration
+                  </li>
+                  <li>
+                    Networking
+                  </li>
+                  <li>
+                    Inauguration of Event
+                  </li>
+                  <li>
+                    Keynote Address
+                  </li>
+                  <li>
+                    Dinner
+                  </li>
+                </ul>
 
+              </div>
+            </div>
+            <div class="event_item_one">
+              <div class="event_flag_bg_img event_item_round">
+                <p class="has-text-white event_day_txt"> Day 2</p>
+                <p class="brand_color event_date_txt">Saturday, August 13</p>
+              </div>
+              <div class="event_list">
+                <ul>
+                  <li>
+                    Mentor Mapping to Teams announced
+                  </li>
+                  <li>
+                    Keynote Address
+                  </li>
+                </ul>
+
+              </div>
+            </div>
+            <div class="event_item_one">
+              <div class="event_flag_bg_img event_item_round">
+                <p class="has-text-white event_day_txt"> Day 3</p>
+                <p class="brand_color event_date_txt">Sunday, August 14</p>
+              </div>
+              <div class="event_list">
+                <ul>
+                  <li>
+                    Shortlisting by mentors
+                  </li>
+                  <li>
+                    Final Evaluation by Judges
+                  </li>
+                  <li>
+                    Award Ceremony
+                  </li>
+                </ul>
+              </div>
+            </div>
+          </div>
+        </div>
+        <div class="column is-9-desktop is-12-mobile is-9-tablet">
+          <img src="@/assets/img/event_sch.png" class="event_sch" />
+        </div>
+      </div>
+      <!-- note -->
+      <div class="event_notes">
+        <h2>Note:</h2>
+        <p> <img src="@/assets/img/hand_pointing.png" class="event_hand_point_img" /> Food and beverages will be taken
+          care by us during the event.
+        </p>
+        <p> <img src="@/assets/img/hand_pointing.png" class="event_hand_point_img" /> For the night stay, please come
+          prepared with air pillows, blankets, toilet kit etc. according to your needs.
+        </p>
+      </div>
+    </div>
+
+  </section>
 </template>
 
 <script>
-export default {
+  export default {
     data() {
-        return {}
+      return {}
     },
     methods: {
 
     }
-}
+  }
+
 </script>
 
 <style>
