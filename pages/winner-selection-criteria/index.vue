f87764fbaaa528e12cd0d9e3bf73ce855d43448b
anandhi2017 <anandhi@systimanx.com>
2022-08-05 16:16:11 +0530

    winner page completed

--- pages/winner-selection-criteria/index.vue
+++ pages/winner-selection-criteria/index.vue
@@ -1,16 +1,84 @@
 <template>
-
+  <section class="winner_selection">
+    <div class="container">
+      <p class="has-text-centered"><img src="@/assets/img/winner.png" class="winner_cup_img" />
+        <span class="winner_text1">
+          Winner Selection Criteria -
+        </span>
+        <span class="winner_text2">Stated in the order of importance starting from highest.</span>
+      </p>
+      <!-- timeline start -->
+      <div class="winner_timeline">
+        <div class="winner_container left">
+          <h2>1.Value</h2>
+          <div class="content">
+            <p>We expect the solution to address the core of the problem. You should be able to elaborate on the core
+              functional and technical features of the given problem and include that in the solution design.</p>
+          </div>
+        </div>
+        <div class="winner_container right">
+          <h2>2.Feasibility</h2>
+          <div class="content">
+            <p>The solution provided is expected to be practical in terms of cost, effort and
+              technical feasibility. Reusing existing solutions in terms of code and building
+              on top of them is very much encouraged. Explore repositories like github and
+              the solutions that are currently used around the world.</p>
+          </div>
+        </div>
+        <div class="winner_container left">
+          <h2>3.User centricity</h2>
+          <div class="content">
+            <p>Consideration of end-user personas in designing the solution maximizes the effectiveness of the solution
+              for adoption in general. Teams are expected to take this perspective into consideration.
+            </p>
+          </div>
+        </div>
+        <div class="winner_container right">
+          <h2>4.Innovation</h2>
+          <div class="content">
+            <p>Viewing problems from different perspectives, Out-of-box thinking and providing unique solution with own
+              logic and algorithms is encouraged.</p>
+          </div>
+        </div>
+        <div class="winner_container left">
+          <h2>5.Clarity in Communications, Presentations</h2>
+          <div class="content">
+            <p>Presentation contents are to be clear, easy to understand and unambiguous. Questions are to be answered
+              with short and relevant answers. You can use Tamil and English liberally in all forms of communications.
+            </p>
+          </div>
+        </div>
+        <div class="winner_container right">
+          <h2>6. Team formation</h2>
+          <div class="content">
+            <p>We encourage team formation with a mix of experienced professionals and students. All professionals team
+              or all students team score lesser than the mixed teams. 4 to 5 person team is encouraged.</p>
+          </div>
+        </div>
+        <div class="winner_container left">
+          <h2>7.Identification of Go To Market Strategy</h2>
+          <div class="content">
+            <p>In order to implement the solution for effective use, next steps in terms of development of features,
+              testing, business planning, etc., are to be identified and articulated as part of Judges evaluation.
+            </p>
+          </div>
+        </div>
+      </div>
+      <!-- timeline end -->
+    </div>
+  </section>
 </template>
 
 <script>
-export default {
+  export default {
     data() {
-        return {}
+      return {}
     },
     methods: {
 
     }
-}
+  }
+
 </script>
 
 <style>
