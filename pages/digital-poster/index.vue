d6e9e0b97d4008947d6ae44b57ca451cb163e68a
anandhi2017 <anandhi@systimanx.com>
2022-08-06 17:27:47 +0530

    register page - completed

--- pages/digital-poster/index.vue
+++ pages/digital-poster/index.vue
@@ -1,16 +1,60 @@
 <template>
-
+  <section class="digi_poster top_space">
+    <div class="container">
+      <h3>
+        The following digital content links are provided for the purpose of
+      </h3>
+      <ul class="">
+        <li>
+          Promoting Madurai Hackathon 2022 event through your business and personal social media channels
+        </li>
+        <li>
+          Distribute digitally to your personal and business network through emails, whatsapp groups, etc.
+        </li>
+        <li>Print and post in notice boards of educational institutions and business organizations</li>
+      </ul>
+      <div class="is-flex poster_download">
+        <p><span class="poster_box_shadow">Poster to print & post in college notice board</span><button
+            class="button is-primary">
+            Download
+          </button></p>
+      </div>
+      <div class="is-flex poster_download">
+        <p><span class="poster_box_shadow">Digital Banner For social Media Pages</span><button
+            class="button is-primary">
+            Download
+          </button></p>
+      </div>
+      <div class="is-flex poster_download">
+        <p><span class="poster_box_shadow">Digital Banner For social Media Pages in tamil</span><button
+            class="button is-primary">
+            Download
+          </button></p>
+      </div>
+      <div class="is-flex poster_download mb_3">
+        <p><span class="poster_box_shadow">Madurai Hackathon 2022 Logo</span><button class="button is-primary">
+            Download
+          </button></p>
+      </div>
+      <!-- <div class="pos_relative">
+        <div class="pos_abs">
+          <img src="@/assets/img/ring.png" alt="Placeholder image">
+        </div>
+      </div> -->
+    </div>
+  </section>
 </template>
 
 <script>
-export default {
+  export default {
     data() {
-        return {}
+      return {}
     },
     methods: {
 
     }
-}
+  }
+
 </script>
 
 <style>
