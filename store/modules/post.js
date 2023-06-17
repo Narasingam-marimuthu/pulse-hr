e8962693e5de2615c583c5495d605850cf69b3ca
rajesh <rajesh-2497>
2023-06-17 12:05:46 +0530

    Merge branch 'master' of https://github.com/Narasingam-marimuthu/pulse-hr

--- store/modules/post.js
+++ store/modules/post.js
@@ -1,26 +1,33 @@
+import { map } from "jquery";
+
 // import baseUrl from "~/config/base.config";
 export default {
     namespaced: true,
     state: {
-        data: []
+        map: Map
     },
     mutations: {
         SET_DATA(state, payload) { 
-            
-            state.data = payload; },
+            state.map = payload;
+        },
         CLEAR_DATA(state) { state.data = {}; }
     },
     actions: {
         async ACTION_POST(vuexContext, inputData) {
             try {
-                let values={};
                 const response = await this.$axios.$post('/upload', inputData.inputDatas)
-                values = Object.values(response.Month);
+                let months = Object.values(response.Month);
+                let flours = Object.values(response['White, self raising flour per 2 kg']);
+                let breads = Object.values(response['Bread, white sliced pan, large (800g)']);
 
-                vuexContext.commit("SET_DATA", values)
-                console.log("Store menu response", values);
+                let map = Map();
+                map.SET_DATA("x", months);
+                map.SET_DATA("y", flours);
+                map.SET_DATA("y1", bread);
+                vuexContext.commit("SET_DATA", map)
+                // console.log("Store menu response", flours);
 
-                return Promise.resolve(JSON.parse(JSON.stringify(values)))
+                return Promise.resolve(JSON.parse(JSON.stringify(map)))
             } catch (error) {
                 console.log("Store menu error", error);
                 if (error.response) {
